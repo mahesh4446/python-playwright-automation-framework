9fe947e0c96c33fb8525eb305ff26cb63498fc3e
Maheshwar Reddy <mahesh4446@users.noreply.github.com>
2026-09-10 01:22:02 +0530

    Create login_page.py

---
+++ tests/pages/login_page.py
@@ -0,0 +1,13 @@
+class LoginPage:
+
+    def __init__(self, page):
+        self.page = page
+
+        self.username = "#username"
+        self.password = "#password"
+        self.login_button = "#login"
+
+    def login(self, username, password):
+        self.page.fill(self.username, username)
+        self.page.fill(self.password, password)
+        self.page.click(self.login_button)
