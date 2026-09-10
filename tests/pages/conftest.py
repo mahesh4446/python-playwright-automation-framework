679fe5a71002e50b3b4bbad8faa620f5486abf1e
Maheshwar Reddy <mahesh4446@users.noreply.github.com>
2026-09-10 08:08:50 +0530

    Update conftest.py

--- tests/pages/conftest.py
+++ tests/pages/conftest.py
@@ -1,11 +1,18 @@
 import pytest
 from playwright.sync_api import sync_playwright
+from config.config import BROWSER, HEADLESS
 
 
 @pytest.fixture(scope="function")
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+
+        browser_type = getattr(p, BROWSER)
+
+        browser = browser_type.launch(
+            headless=HEADLESS
+        )
+
         page = browser.new_page()
 
         yield page
